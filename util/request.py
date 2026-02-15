ade3fcad2f02972a3ad96b24d159c3f71dc5af15
RainChen0920 <Rainchen975420@gmail.com>
2026-02-15 00:39:07 -0500

    fifth commit, need to handle large get request containing cookies

--- util/request.py
+++ util/request.py
@@ -18,7 +18,7 @@ class Request:
 
         self.headers = {}
         for header in headers:
-            if (not header.strip()) or (b":" not in header):
+            if (not header.strip()) or (b":" not in header):  # badly formed headers
                 continue
             x = header.split(b':', 1)    
             key = x[0].decode().strip()
@@ -27,16 +27,22 @@ class Request:
             for x in key.lower().split('-'):
                 parts.append(x.capitalize())
             capitalizedKey = '-'.join(parts)
-            self.headers[capitalizedKey] = value   
+            if (capitalizedKey == "Cookie") and ("Cookie" in self.headers):
+                self.headers["Cookie"] = self.headers["Cookie"] + "; " + value
+            else:
+                self.headers[capitalizedKey] = value  
 
         self.cookies = {}
         if "Cookie" in self.headers:
             cookies = self.headers["Cookie"].split(';')
             for header in cookies:
-                y = header.split('=', 1)
-                if len(y) != 2:
+                header = header.strip()
+                if (not header) or ("=" not in header):
                     continue
-                key = y[0].lower().strip()
+                y = header.split('=', 1)
+                key = y[0].strip()
+                if not key:  # in case of key being an empty string
+                    continue
                 value = y[1].strip()  
                 self.cookies[key] = value
 
@@ -122,6 +128,7 @@ def test6():  # case of extra CRLF at the end
 
 def test7():  # case of cookie value with = in it
     req = Request(b"GET /cookie HTTP/1.1\r\nHost: example.com\r\nCookie: lotofequal=abc=123==; theme=dark=; id=67\r\n\r\n")
+    print(req.headers)
     assert req.cookies["lotofequal"] == "abc=123=="
     assert req.cookies["theme"] == "dark="
     assert req.cookies["id"] == "67"
@@ -129,11 +136,25 @@ def test7():  # case of cookie value with = in it
 
 def test8():  # case of extra spaces around cookie names and values
     req = Request(b"GET /cookie HTTP/1.1\r\nHost: example.com\r\nCookie: a=1;  b=2 ; c = three;    d=four\r\n\r\n")
+    print(req.headers)
     assert req.cookies["a"] == "1"
     assert req.cookies["b"] == "2"
     assert req.cookies["c"] == "three"
     assert req.cookies["d"] == "four"
     #print("test 8 good") 
+
+def test9():  # case of multiple cookie headers
+    req = Request(b"GET / HTTP/1.1\r\nHost: x\r\nCookie: a=1\r\nCookie: b=2\r\n\r\n")
+    print(req.headers)
+    assert req.cookies["a"] == "1"
+    assert req.cookies["b"] == "2"
+
+def test10():
+    req = Request(b"POST /test HTTP/1.1\r\nHost: x\r\nCookie: a=1; b=2\r\nContent-Length: 5\r\n\r\nhello")
+    assert req.method == "POST"
+    assert req.cookies["a"] == "1"
+    assert req.cookies["b"] == "2"
+    assert req.body == b"hello"
 
 if __name__ == '__main__':
     test1()
@@ -144,3 +165,5 @@ if __name__ == '__main__':
     test6()
     test7()
     test8()
+    test9()
+    test10()
